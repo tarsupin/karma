c55f1f8cebc72b23fa01fa9e82c35d173979da3d
brint <brint@blendimc.com>
2014-11-22 09:33:24 -0600

    Mobile Menu Update

--- includes/global.php
+++ includes/global.php
@@ -14,7 +14,7 @@ require(SYS_PATH . "/controller/includes/social-menu.php");
 require(SYS_PATH . "/controller/includes/uni-menu.php");
 
 // Main Navigation
-WidgetLoader::add("SidePanel", 10, '
+WidgetLoader::add("MobilePanel", 10, '
 <div class="panel-box">
 	<ul class="panel-slots">
 		<li class="nav-slot"><a href="/">Home<span class="icon-circle-right nav-arrow"></span></a></li>
